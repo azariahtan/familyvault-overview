d7fdd11a53b0bab3b2ae660c001cf0d5dd20261b
azariahtan <azariahtan.piano@gmail.com>
2026-05-26 09:45:30 +0800

    Create main.tsx

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import "../src/styles.css";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "@tanstack/react-router";
+import { getRouter } from "./router";
+
+const router = getRouter();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
